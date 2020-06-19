cdd238d13125dab6a092b5022eca482359e27d74
Fabiana gomes <fabiana.gomes@ccc.ufcg.edu.br>
2020-06-18 23:08:59 -0300

    Adding code do maintain page

---
+++ react_page/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './views/Dashboard';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
