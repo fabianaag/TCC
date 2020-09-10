dfaf4d73d3a08dbbfa97bcbfa24b35e11ff7f33c
Fabiana Gomes <fabianaag@ccc.ufcg.edu.br>
2020-09-09 21:03:42 -0300

    Updates

---
+++ precache-manifest.d66a551a67e5a4490e04bc79ddbdd3d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac48374a0ab7b0393678c17b8058cc9d",
+    "url": "/TCC/index.html"
+  },
+  {
+    "revision": "b057f7a19e09b983e451",
+    "url": "/TCC/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d408ace5cc4980266d90",
+    "url": "/TCC/static/js/2.70b656c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TCC/static/js/2.70b656c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b057f7a19e09b983e451",
+    "url": "/TCC/static/js/main.4cfba2b2.chunk.js"
+  },
+  {
+    "revision": "79ff5eb0dfd6ed5615f4",
+    "url": "/TCC/static/js/runtime-main.de9d69a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TCC/static/media/logo.5d5d9eef.svg"
+  }
+]);
